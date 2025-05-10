8e93c5fb795025559b374fd4d84ee111682f2ff8
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-05-10 12:29:39 -0400

    add function to read a book into memory

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    ret_string = ""
+    with open(filepath) as file:
+        ret_string = file.read()
+    return ret_string
+
+def main():
+    frankenstein_text = get_book_text("./books/frankenstein.txt")
+    print(frankenstein_text)
+
+main()
