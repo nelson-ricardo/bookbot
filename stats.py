5e6afb3a4033d4a4f22a32e51a260a15ec6bb638
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-05-11 11:08:21 -0400

    finish function to return a list of dictionaries

--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+# function used in sort_dictionary
+def key_func(e):
+    return e[1]
+
 def word_counter(text):
     word_count = len(text.split())
     return word_count
@@ -15,5 +19,13 @@ def char_counter(text):
     return ret_dict
 
 def sort_dictionary(unsorted_dict):
-    list_dict = unsorted_dict.items()
-    return list_dict
+    list_dict = list(unsorted_dict.items())
+    # sort by the the number of times an item appears in the list
+    list_dict.sort(reverse=True, key=key_func)
+    
+    # create a list of dictionary
+    ret_list = []
+    for item in list_dict:
+        ret_list.append({item[0]:item[1]})
+    
+    return ret_list
