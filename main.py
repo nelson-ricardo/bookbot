ab3d27d15c7c037a74e529730a2d7a7b32d976ca
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-05-11 11:01:02 -0400

    refactored char_counter and add sort_dict function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter
+from stats import word_counter, char_counter, sort_dictionary
 
 def get_book_text(filepath):
     ret_string = ""
@@ -10,5 +10,10 @@ def main():
     frankenstein_text = get_book_text("./books/frankenstein.txt")
     word_count = word_counter(frankenstein_text)
     print(f"{word_count} words found in the document")
+    char_count = char_counter(frankenstein_text)
+    # for key in char_count:
+    #     print(f"'{key}': {char_count[key]}")
+    list_dict = sort_dictionary(char_count)
+    print(list_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def word_counter(text):
     word_count = len(text.split())
     return word_count
+
+def char_counter(text):
+    ret_dict = {}
+
+    for char in text:
+        lowered_char = char.lower()
+        if lowered_char in ret_dict:
+            ret_dict[lowered_char] += 1
+        else:
+            ret_dict[lowered_char] = 1
+    
+    return ret_dict
+
+def sort_dictionary(unsorted_dict):
+    list_dict = unsorted_dict.items()
+    return list_dict
